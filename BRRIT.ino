81131b1af9fc6db9ad0270106051f01de7c1aa90
Rafi Khan <zetoslab@gmail.com>
2017-04-09 01:51:56 -0600

    initial

---
+++ BRRIT.ino
@@ -0,0 +1,40 @@
+// BRRIT sample code
+// by Rafi Khan <zetoslab@gmail.com>
+
+/*
+ * Description:
+ * 
+ * The robot should be able to move around freely and 
+ * turn left when it finds and object within a range of 10 cm
+ * 
+ * 
+ * Parts Used:
+ * L298N Dual Motor Controller <http://www.instructables.com/id/Control-DC-and-stepper-motors-with-L298N-Dual-Moto/>
+ * HC-SR04 <http://playground.arduino.cc/Code/NewPing>, <https://bitbucket.org/teckel12/arduino-new-ping/wiki/Home>
+ */
+
+ // Variables
+
+ // Motor:
+ int motor_left_pin = 6;
+ int motor_right_pin = 7;
+
+ // Ping:
+
+void setup() {
+  // Initialise motors
+
+  // Initialise PING sensor
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+
+void turnLeft() {
+  
+}
+
